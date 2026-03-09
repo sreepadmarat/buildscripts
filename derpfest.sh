8eb403f949b6d2a2942f538e6d72955c3b4cc405
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-03-09 12:16:14 +0530

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -4,9 +4,9 @@ rm -rf .repo/local_manifests
 
 repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs
 
-/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh
 #safer sync
-/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 rm -rf device/oneplus/larry
 rm -rf device/oneplus/sm6375-common
@@ -14,7 +14,6 @@ rm -rf hardware/oplus
 rm -rf kernel/oneplus/sm6375
 rm -rf vendor/oneplus/larry
 rm -rf vendor/oneplus/sm6375-common
-rm -rf out/target/product/larry
 
 git clone https://github.com/sreepadmarat/android_device_oneplus_larry device/oneplus/larry -b derpfest --depth 1
 git clone https://github.com/sreepadmarat/android_device_oneplus_sm6375-common device/oneplus/sm6375-common -b derpfest --depth 1
