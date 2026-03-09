497a2734a5b7fd38180ae7638ddb46b324844a24
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-03-09 11:27:55 +0530

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -22,7 +22,11 @@ git clone https://github.com/sreepadmarat/android_hardware_oplus hardware/oplus 
 git clone https://github.com/LineageOS/android_kernel_oneplus_sm6375 kernel/oneplus/sm6375 -b lineage-23.2 --depth 1
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_larry vendor/oneplus/larry -b lineage-23.2 --depth 1
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sm6375-common vendor/oneplus/sm6375-common -b lineage-23.2 --depth 1
-svn export https://github.com/sreepadmarat/buildscripts/main/keys device/oneplus/larry/keys
+
+mkdir -p device/oneplus/larry/keys
+for file in Android.bp bluetooth.pk8 bluetooth.x509.pem BUILD cts_uicc_2021.pk8 cts_uicc_2021.x509.pem gmscompat_lib.pk8 gmscompat_lib.x509.pem keys.sh make_key.sh media.pk8 media.x509.pem networkstack.pk8 networkstack.x509.pem nfc.pk8 nfc.x509.pem platform.pk8 platform.x509.pem releasekey.pk8 releasekey.x509.pem sdk_sandbox.pk8 sdk_sandbox.x509.pem shared.pk8 shared.x509.pem testkey.pk8 testkey.x509.pem verifiedboot.pk8 verifiedboot.x509.pem; do
+  curl -sL "https://raw.githubusercontent.com/sreepadmarat/buildscripts/main/keys/$file" -o "device/oneplus/larry/keys/$file"
+done
 
 repo forall -c 'git lfs pull'
 
