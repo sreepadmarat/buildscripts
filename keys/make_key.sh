56244425b96700a871ac2a7ac065d7e5aef4bb88
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-06-07 21:31:31 +0530

    Add files via upload

---
+++ keys/make_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+set -u
+bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
+    $1 \
+    '/C=IN/ST=Karnataka/L=Bengaluru/O=Android/OU=Android/CN=Android/emailAddress=sreepad.marat.com'
