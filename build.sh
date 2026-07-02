6c6879f442aed1ddbae11e71c5e0a2fac443b811
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-07-02 20:05:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,7 +31,6 @@ git clone -b lineage-23.2 https://github.com/sreepadmarat/device_oneplus_sm6375-
 git clone -b pixel https://github.com/sreepadmarat/hardware_oplus hardware/oplus --depth=1
 git clone -b 16.2 https://github.com/Larry-ROM-Archive/hardware_dolby hardware/dolby --depth=1
 git clone -b 16.0 https://github.com/Larry-ROM-Archive/packages_apps_GameBar packages/apps/GameBar --depth=1
-git clone -b 16.1 https://github.com/diathequeen/proprietary_oneplus_oos_camera vendor/oplus/camera --depth=1
 git clone -b 16.0-hybrid https://github.com/sreepadmarat/android_kernel_oneplus_sm6375 kernel/oneplus/sm6375 --depth=1
 git clone -b lineage-23.2 https://github.com/sreepadmarat/proprietary_vendor_oneplus_larry vendor/oneplus/larry --depth=1
 git clone -b lineage-23.2 https://github.com/sreepadmarat/proprietary_vendor_oneplus_sm6375-common vendor/oneplus/sm6375-common --depth=1
