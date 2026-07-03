1d90ecc25da3a4f02d9ec0d96f95fb135f8852d8
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-07-03 05:40:58 +0530

    Refactor build_gms.sh for updated repo structure
    Updated repository initialization and cloning commands to use new branches and paths. Adjusted symlink and key paths for build scripts.

--- Infinity-X/build_gms.sh
+++ Infinity-X/build_gms.sh
@@ -14,7 +14,8 @@ rm -rf vendor/lineage-priv
 rm -rf first
 
 # 2. Rom source repo initialization
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefaultecho "=================="
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+echo "=================="
 echo "Repo init success"
 echo "=================="
 
@@ -30,9 +31,9 @@ git clone -b infinity https://github.com/sreepadmarat/device_oneplus_sm6375-comm
 git clone -b infinity https://github.com/sreepadmarat/hardware_oplus hardware/oplus --depth=1
 git clone -b 16.2 https://github.com/Larry-ROM-Archive/hardware_dolby hardware/dolby --depth=1
 git clone -b 16.0 https://github.com/Larry-ROM-Archive/packages_apps_GameBar packages/apps/GameBar --depth=1
-git clone -b 16.0 https://github.com/Larry-ROM-Archive/kernel_oneplus_sm6375 kernel/oneplus/sm6375 --depth=1
-git clone -b 16.2 https://github.com/Larry-ROM-Archive/vendor_oneplus_larry vendor/oneplus/larry --depth=1
-git clone -b 16.2 https://github.com/Larry-ROM-Archive/vendor_oneplus_sm6375-common vendor/oneplus/sm6375-common --depth=1
+git clone -b 16.0-hybrid https://github.com/sreepadmarat/kernel_oneplus_sm6375 kernel/oneplus/sm6375 --depth=1
+git clone -b lineage-23.2 https://github.com/TheMuppets/proprietary_vendor_oneplus_larry vendor/oneplus/larry --depth=1
+git clone -b lineage-23.2 https://github.com/TheMuppets/proprietary_vendor_oneplus_sm6375-common vendor/oneplus/sm6375-common --depth=1
 echo "============ Custom Trees Cloned Successfully ==============="
 
 # Patches
@@ -51,9 +52,9 @@ repo forall -c 'git lfs pull'
 # 6. Clean Signing Keys & absolute path Symlinking
 mkdir -p vendor/infinity-priv
 git clone --depth 1 https://github.com/sreepadmarat/buildscripts.git vendor/infinity-priv/buildscripts_tmp
-mv vendor/lineage-priv/buildscripts_tmp/keys vendor/infinity-priv/keys
+mv vendor/infinity-priv/buildscripts_tmp/keys vendor/infinity-priv/keys
 rm -rf vendor/infinity-priv/buildscripts_tmp
-ln -s infinity-priv vendor/lineage-priv
+sed -i 's|vendor/lineage-priv/keys/|vendor/infinity-priv/keys/|g' vendor/infinity-priv/keys/keys.mk
 
 # Export environmental variables
 export TZ=Asia/Kolkata
@@ -64,10 +65,10 @@ export WITH_GAPPS=true
 echo "======= Export Done ======"
 
 # Lunch 
-lunch infinity_larry-bp4a-userdebug 
+lunch infinity_larry-bp4a-userdebug
 echo "====== Lunch Set ======="
+
 m installclean
 m bacon
 mkdir -p first && cp out/target/product/larry/Project_Infinity-X*.zip out/target/product/larry/boot.img out/target/product/larry/vendor_boot.img out/target/product/larry/dtbo.img out/target/product/larry/system/build.prop first/
-m installclean
 m updatepackage
