afeee513cb93594698e7b5c9d5938c7b5c326756
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-07-28 18:08:39 +0530

    Refactor build script for vendor management and syncing
    Removed old vendor cleanup commands and added new project entries with clone-depth for better performance.

--- build.sh
+++ build.sh
@@ -9,18 +9,13 @@ rm -rf vendor/oneplus/larry
 rm -rf vendor/oneplus/sm6375-common
 rm -rf vendor/lineage-priv
 rm -rf .repo/local_manifests/custom.xml
+rm -rf vendor/oplus/camera
 
 # 2. Rom source repo initialization
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
-
-# 3. Sync the base platform repositories FIRST
-/opt/crave/resync.sh
-# For Safety
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-echo "============ Base Repo Sync Successfull ==============="
 
 # Ensure local_manifests directory exists
 mkdir -p .repo/local_manifests
@@ -32,26 +27,30 @@ cat << 'EOF' > .repo/local_manifests/custom.xml
   <remote name="gitlab" fetch="https://gitlab.com/" />
   <remote name="github" fetch="https://github.com/" />
 
-  <project path="vendor/pixel/gms" name="thecloverproject/vendor_pixel_gms" remote="gitlab" revision="16-qpr2" />
-  <project path="vendor/pixel/launcher" name="thecloverproject/vendor_pixel_launcher" remote="gitlab" revision="16-qpr2" />
-  <project path="vendor/pixel/themepicker" name="thecloverproject/vendor_pixel_themepicker" remote="gitlab" revision="16-qpr2" />
-  <project path="vendor/pixel/sounds" name="thecloverproject/vendor_pixel_sounds" remote="gitlab" revision="16-qpr2" />
-  
-  <project path="vendor/pixel-style" name="Evolution-X/vendor_pixel-style" remote="github" revision="bka" />
-  <project path="vendor/gms_spoof" name="Neoteric-OS/android_vendor_gms_spoof" remote="github" revision="master" />
+  <project path="vendor/pixel/gms" name="thecloverproject/vendor_pixel_gms" remote="gitlab" revision="16-qpr2" clone-depth="1" />
+  <project path="vendor/pixel/launcher" name="thecloverproject/vendor_pixel_launcher" remote="gitlab" revision="16-qpr2" clone-depth="1" />
+  <project path="vendor/pixel/themepicker" name="thecloverproject/vendor_pixel_themepicker" remote="gitlab" revision="16-qpr2" clone-depth="1" />
+  <project path="vendor/pixel/sounds" name="thecloverproject/vendor_pixel_sounds" remote="gitlab" revision="16-qpr2" clone-depth="1" />
+  <project path="vendor/pixel-style" name="Evolution-X/vendor_pixel-style" remote="github" revision="bka" clone-depth="1" />
+  <project path="vendor/gms_spoof" name="Neoteric-OS/android_vendor_gms_spoof" remote="github" revision="master" clone-depth="1" />
+
+  <project path="vendor/oplus/camera" name="sreepadmarat/vendor-oplus-camera" remote="github" revision="lineage-23.2" clone-depth="1" />
+  <project path="device/oneplus/larry" name="sreepadmarat/android_device_oneplus_larry" remote="github" revision="los" clone-depth="1" />
+  <project path="device/oneplus/sm6375-common" name="sreepadmarat/android_device_oneplus_sm6375-common" remote="github" revision="los" clone-depth="1" />
+  <project path="hardware/oplus" name="LineageOS/android_hardware_oplus" remote="github" revision="lineage-23.2" clone-depth="1" />
+  <project path="kernel/oneplus/sm6375" name="sreepadmarat/android_kernel_oneplus_sm6375" remote="github" revision="16.2-resukisu" clone-depth="1" />
+  <project path="vendor/oneplus/larry" name="TheMuppets/vendor_oneplus_larry" remote="github" revision="lineage-23.2" clone-depth="1" />
+  <project path="vendor/oneplus/sm6375-common" name="TheMuppets/vendor_oneplus_sm6375-common" remote="github" revision="lineage-23.2" clone-depth="1" />
 </manifest>
 EOF
 
 echo "=== Manifest custom.xml created successfully! ==="
 
-# 4. NOW inject your custom device trees (Safe from being pruned!)
-git clone -b los https://github.com/sreepadmarat/android_device_oneplus_larry device/oneplus/larry --depth=1
-git clone -b los https://github.com/sreepadmarat/android_device_oneplus_sm6375-common device/oneplus/sm6375-common --depth=1
-git clone -b lineage-23.2 https://github.com/LineageOS/android_hardware_oplus hardware/oplus --depth=1
-git clone -b 16.2-resukisu https://github.com/sreepadmarat/android_kernel_oneplus_sm6375 kernel/oneplus/sm6375 --depth=1
-git clone -b lineage-23.2 https://github.com/TheMuppets/vendor_oneplus_larry vendor/oneplus/larry --depth=1
-git clone -b lineage-23.2 https://github.com/TheMuppets/vendor_oneplus_sm6375-common vendor/oneplus/sm6375-common --depth=1
-echo "============ Custom Trees Cloned Successfully ==============="
+# 3. Sync the base platform repositories FIRST
+/opt/crave/resync.sh
+# For Safety
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+echo "============ Base Repo Sync Successfull ==============="
 
 # Download lfs Artifacts
 repo forall -c 'git lfs pull'
