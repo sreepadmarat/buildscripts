a633ced1f86fe80b5fb72906f39d50b9001445eb
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-08-13 14:43:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ cat << 'EOF' > .repo/local_manifests/custom.xml
   <project path="device/oneplus/larry" name="sreepadmarat/android_device_oneplus_larry" remote="github" revision="los" clone-depth="1" />
   <project path="device/oneplus/sm6375-common" name="sreepadmarat/android_device_oneplus_sm6375-common" remote="github" revision="los" clone-depth="1" />
   <project path="hardware/oplus" name="LineageOS/android_hardware_oplus" remote="github" revision="lineage-23.2" clone-depth="1" />
-  <project path="kernel/oneplus/sm6375" name="sreepadmarat/android_kernel_oneplus_sm6375" remote="github" revision="lineage-23.2" clone-depth="1" />
+  <project path="kernel/oneplus/sm6375" name="sreepadmarat/android_kernel_oneplus_sm6375" remote="github" revision="kprofiles" clone-depth="1" />
   <project path="vendor/oneplus/larry" name="TheMuppets/proprietary_vendor_oneplus_larry" remote="github" revision="lineage-23.2" clone-depth="1" />
   <project path="vendor/oneplus/sm6375-common" name="TheMuppets/proprietary_vendor_oneplus_sm6375-common" remote="github" revision="lineage-23.2" clone-depth="1" />
   <project path="packages/apps/TouchServices" name="sreepadmarat/packages_apps_TouchServices" remote="github" revision="lineage-23.2" clone-depth="1" />
