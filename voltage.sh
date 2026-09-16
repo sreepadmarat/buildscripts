b664fa01cf281cef7b3d3ab27afcb3555126d733
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-09-17 05:11:38 +0530

    Refactor voltage.sh for cleanup and new clone command
    Removed redundant command to remove keys directory and added command to clone build_soong repository.

--- voltage.sh
+++ voltage.sh
@@ -7,7 +7,7 @@
     rm -rf kernel/oneplus/sm6375                                                                                                                                                                  
     rm -rf vendor/oneplus/larry                                                                                                                                                                   
     rm -rf vendor/oneplus/sm6375-common                                                                                                                                                           
-    rm -rf vendor/voltage-priv/keys                                                                                                                                                                
+    rm -rf vendor/voltage-priv/keys
                                                                                                                                                                                                   
     # 2. Initialize Custom ROM / ASCP Manifest                                                                                                                                                    
     repo init -u https://github.com/VoltageOS/manifest.git -b 17 --git-lfs --depth=1                                                                                                     
@@ -18,8 +18,9 @@
     # 3. Sync the base platform repositories FIRST                                                                                                                                                
     /opt/crave/resync.sh
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+    rm -rf build/soong
     echo "============ Base Repo Sync Successful ==============="
-  
+    
     # 4. Clone adapted VoltageOS trees (--depth=1 for fast cloning)
     git clone --depth=1 -b voltage https://github.com/sreepadmarat/android_device_oneplus_larry.git device/oneplus/larry
     git clone --depth=1 -b voltage https://github.com/sreepadmarat/android_device_oneplus_sm6375-common.git device/oneplus/sm6375-common
@@ -27,6 +28,7 @@
     git clone --depth=1 -b voltage https://github.com/sreepadmarat/android_kernel_oneplus_sm6375.git kernel/oneplus/sm6375
     git clone --depth=1 -b voltage https://github.com/sreepadmarat/proprietary_vendor_oneplus_larry.git vendor/oneplus/larry
     git clone --depth=1 -b voltage https://github.com/sreepadmarat/proprietary_vendor_oneplus_sm6375-common.git vendor/oneplus/sm6375-common
+    git clone --depth=1 -b 17 https://github.com/sreepadmarat/build_soong.git build/soong
   
     # 5. Download Git LFS Artifacts
     repo forall -c 'git lfs pull'
